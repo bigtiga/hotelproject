db2ec23f1f517493dc7266955b4e56ccabbea484
Festus ikpefua <festigawork@gmail.com>
2026-08-19 00:16:47 -0400

    Initial commit: Hotel Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+const App = ()=>{
+  
+  
+  return (
+    
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}>
+      <h1>🏨 Welcome to Waje Hotel</h1>
+      <p>Your domain is working! Site under construction.</p>
+    </div>
+  )
+
+}
+export default App;
